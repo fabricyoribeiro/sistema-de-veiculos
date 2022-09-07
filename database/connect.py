2fb57e436c5258fd0bb10ed76ae40dce4f3be0ee
Fabricyo Ribeiro <69694301+fabricyoribeiro@users.noreply.github.com>
2022-09-07 19:56:14 -0300

    .

--- database/connect.py
+++ database/connect.py
@@ -4,7 +4,7 @@ class ConnectDataBase:
     def __init__(self):
         self._connect = psycopg2.connect(
             host="localhost",
-            database="controle_gastos_veiculos_2",
+            database="controle_gastos_veiculos",
             user="postgres",
             password="18102002"
         )
